77a93d5195c5f7862db2bb9a064b0289caeec9af
Kaare Rasmussen <kaare@jasonic.dk>
2017-12-01 03:29:06 +0100

    Upgrade to V3 as defined here https://app.pakkelabels.dk/api/public/v3/specification

--- lib/P5kkelabels.pm
+++ lib/P5kkelabels.pm
@@ -3,154 +3,12 @@ package P5kkelabels;
 use 5.010;
 
 use Moo;
-with 'Role::REST::Client';
+with qw/
+    Role::REST::Client
+    Role::REST::Client::Auth::Basic
+/;
 
-use constant API_ENDPOINT => 'https://app.pakkelabels.dk/api/public/v2/';
-
-=head1 ATTRIBUTES
-
-=cut
-
-has 'api_user' => (
-    is => 'ro',
-);
-
-has 'api_key' => (
-    is => 'ro',
-);
-
-has 'token' => (
-    is => 'ro',
-    default => sub {
-		my $self = shift;
-        $self->server(API_ENDPOINT);
-        my $result = $self->post('users/login', {api_user => $self->api_user, api_key => $self->api_key});
-        return $result->data->{'token'};
-    },
-    lazy => 1,
-);
-
-
-=head1 METHODS
-
-=cut
-
-sub _get  {
-    my ( $self, $path, $data ) = @_;
-    $data //= {};
-    $data->{token} = $self->token;
-    $self->get($path, $data);
-};
-
-sub _post  {
-    my ( $self, $path, $data ) = @_;
-    $data //= {};
-    $data->{token} = $self->token;
-    $self->post($path, $data);
-};
-
-sub balance {
-    my $self = shift;
-    my $result = $self->_get('users/balance');
-    return $result->data->{balance};
-}
-
-sub pdf {
-    my ($self, $id) = @_;
-    my $result = $self->_get('shipments/pdf', {id => $id});
-    return $result->data->{base64};
-}
-
-sub zpl {
-    my ($self, $id) = @_;
-    my $result = $self->_get('shipments/zpl', {id => $id});
-    return $result->data->{base64};
-}
-
-sub shipments {
-    my ($self, $params) = @_;
-    my $result = $self->_get('shipments/shipments', $params);
-    return $result->data;
-}
-
-sub imported_shipments {
-    my ($self, $params) = @_;
-    my $result = $self->_get('shipments/imported_shipments', $params);
-    return $result->data;
-}
-sub create_imported_shipment {
-    my ($self, $params) = @_;
-    my $result = $self->_post('shipments/imported_shipment', $params);
-    return $result->data;
-}
-
-sub create_shipment {
-    my ($self, $params) = @_;
-    my $result = $self->_post('shipments/shipment', $params);
-    return $result->data;
-}
-
-sub create_shipment_own_customer_number {
-    my ($self, $params) = @_;
-    my $result = $self->_post('shipments/shipment_own_customer_number', $params);
-    return $result->data;
-}
-
-sub freight_rates {
-    my ($self, $params) = @_;
-    my $result = $self->_get('shipments/freight_rates', $params);
-    return $result->data;
-}
-
-sub payment_requests {
-    my $self = shift;
-    my $result = $self->_get('users/payment_requests');
-    return $result->data;
-}
-
-sub gls_droppoints {
-    my ($self, $params) = @_;
-    my $result = $self->_get('shipments/gls_droppoints', $params);
-    return $result->data;
-}
-
-sub pdk_droppoints {
-    my ($self, $params) = @_;
-    my $result = $self->_get('shipments/pdk_droppoints', $params);
-    return $result->data;
-}
-
-sub dao_droppoints {
-    my ($self, $params) = @_;
-    my $result = $self->_get('shipments/dao_droppoints', $params);
-    return $result->data;
-}
-
-sub pickup_points {
-    my ($self, $params) = @_;
-    my $result = $self->_get('pickup_points', $params);
-    return $result->data;
-}
-
-sub add_to_print_queue {
-    my ($self, $shipments) = @_;
-    my $result = $self->_post('shipments/add_to_print_queue', {ids => join(',', @$shipments)});
-    return $result->data;
-}
-
-sub pdf_multiple {
-    my ($self, $shipments) = @_;
-    my $result = $self->_get('shipments/pdf_multiple', {ids => join(',', @$shipments)});
-    return $result->data;
-}
-
-1;
-
-__END__
-
-# ABSTRACT: API interface to pakkelabels.dk
-
-=pod
+use constant API_ENDPOINT => 'https://app.pakkelabels.dk/api/public/v3/';
 
 =head1 NAME
 
@@ -160,9 +18,244 @@ P5kkelabels - REST API interface
 
 =head1 DESCRIPTION
 
+Implements the Pakkelabels.dk API as described in
+https://app.pakkelabels.dk/api/public/v3/specification
+
+All methods return a L<Role::REST::Client::Result> object.
+
 =head1 METHODS
 
-=head2 methods
+=head2 products
+
+Get available products
+
+=cut
+
+sub products {
+    my ($self, $params) = @_;
+    my $result = $self->get(API_ENDPOINT . 'products', $params);
+    return $result;
+}
+
+=head2 pickup_points
+
+Get available & nearest pickup points
+
+=cut
+
+sub pickup_points {
+    my ($self, $params) = @_;
+    my $result = $self->get(API_ENDPOINT . 'pickup_points', $params);
+    return $result;
+}
+
+=head2 account_balance
+
+Get current balance
+
+=cut
+
+sub account_balance {
+    my $self = shift;
+    my $result = $self->get(API_ENDPOINT . 'account/balance');
+    return $result;
+}
+
+=head2 account_payment_request
+
+Get payment requests
+
+=cut
+
+sub account_payment_requests {
+    my ($self, $params) = @_;
+    my $result = $self->get(API_ENDPOINT . 'account/payment_requests', $params);
+    return $result;
+}
+
+=head2 shipment_monitor
+
+Get shipment monitor statuses
+
+=cut
+
+sub shipment_monitor {
+    my ($self, $params) = @_;
+    my $result = $self->get(API_ENDPOINT . 'shipment_monitor_statuses', $params);
+    return $result;
+}
+
+=head2 return_portals
+
+Get return portals
+
+Takes an optional id parameter
+
+=cut
+
+sub return_portals {
+    my ($self, $params) = @_;
+    my $path = 'return_portals';
+    $path .= "/$params->{id}" if defined $params and exists $params->{id};
+    my $result = $self->get(API_ENDPOINT . $path);
+    return $result;
+}
+
+=head2 return_portal_shipments
+
+Get Shipments for Return Portal with specific ID
+
+Takes an id parameter
+
+=cut
+
+sub return_portal_shipments {
+    my ($self, $params) = @_;
+    die "No id provided" unless my $id = $params->{id};
+
+    my $path = "return_portals/$id/shipments";
+    my $result = $self->get(API_ENDPOINT . $path, $params);
+    return $result;
+}
+
+=head2 shipments
+
+Get shipments
+
+Takes an optional id parameter
+
+=cut
+
+sub shipments {
+    my ($self, $params) = @_;
+    my $path = 'shipments';
+    $path .= "/$params->{id}" if defined $params and exists $params->{id};
+
+    my $result = $self->get(API_ENDPOINT . $path);
+    return $result;
+}
+
+=head2 create_shipment
+
+Create a shipment
+
+Takes the sipment information as parameter
+
+=cut
+
+sub create_shipment {
+    my ($self, $params) = @_;
+    my $result = $self->post(API_ENDPOINT . 'shipments', $params);
+    return $result;
+}
+
+=head2 shipment_labels
+
+Get Labels for Shipment with specific ID
+
+Takes an id parameter
+
+=cut
+
+sub shipment_labels {
+    my ($self, $params) = @_;
+    die "No id provided" unless $params->{id};
+
+    my $path = "shipments/$params->{id}/labels";
+    my $result = $self->get(API_ENDPOINT . $path);
+    return $result;
+}
+
+=head2 print_queue_entries
+
+Get print queue entries
+
+=cut
+
+sub print_queue_entries {
+    my ($self, $params) = @_;
+    my $result = $self->get(API_ENDPOINT . 'print_queue_entries');
+    return $result;
+}
+
+=head2 imported_shipments
+
+Get Imported Shipments
+
+Takes an id parameter
+
+=cut
+
+sub imported_shipments {
+    my ($self, $params) = @_;
+    my $path = 'imported_shipments';
+    $path .= "/$params->{id}" if defined $params and exists $params->{id};
+    my $result = $self->get(API_ENDPOINT . $path);
+    return $result;
+}
+
+=head2 create_imported_shipment
+
+Create a shipment
+
+Takes the sipment information as parameter
+
+=cut
+
+sub create_imported_shipment {
+    my ($self, $params) = @_;
+    my $result = $self->post(API_ENDPOINT . 'imported_shipments', $params);
+    return $result;
+}
+
+=head2 update_imported_shipment
+
+Update a shipment
+
+Takes the sipment information as parameter
+
+=cut
+
+sub update_imported_shipment {
+    my ($self, $params) = @_;
+    my $result = $self->put(API_ENDPOINT . 'imported_shipments', $params);
+    return $result;
+}
+
+=head2 delete_imported_shipment
+
+Delete a shipment
+
+Takes the sipment information as parameter
+
+=cut
+
+sub delete_imported_shipment {
+    my ($self, $params) = @_;
+    my $result = $self->delete(API_ENDPOINT . 'imported_shipments', $params);
+    return $result;
+}
+
+=head2 labels
+
+Get Labels for specific ID's
+
+=cut
+
+sub labels {
+    my ($self, $params) = @_;
+    my $path = "labels";
+    my $result = $self->get(API_ENDPOINT . $path, $params);
+    return $result;
+}
+
+1;
+
+__END__
+
+# ABSTRACT: API interface to pakkelabels.dk
+
+=pod
 
 =head1 BUGS
 
